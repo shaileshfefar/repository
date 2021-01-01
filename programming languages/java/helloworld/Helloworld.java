4576ac2f2693e27966e6498d4ad85907d0c0bda1
Shailesh Fefar <salu1912@gmail.com>
2021-01-01 18:17:27 +0530

    Create java/helloworld/Helloworld.java file...

--- programming languages/java/helloworld/Helloworld.java
+++ programming languages/java/helloworld/Helloworld.java
@@ -1,10 +1,10 @@
 /*
-	@Program name: Print "Helloworld! How are you?" on console
-	@Date: 05/12/2012
-	@Author: Shailesh Fefar
-	@File name: Helloworld.java
-	@Version: Java 1.8
-	@Licence Type: MIT License
+  @Program name: Print "Helloworld! How are you?" on console
+  @Date: 05/12/2012
+  @Author: Shailesh Fefar
+  @File name: Helloworld.java
+  @Version: Java 1.8
+  @Licence Type: MIT License
 */
 // Class Helloworld
 public class Helloworld

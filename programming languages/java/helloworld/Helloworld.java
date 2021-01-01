bb852b2df42cdf88e18aa5e30c6a2ab0cd40bc6c
Shailesh Fefar <salu1912@gmail.com>
2021-01-01 17:39:29 +0530

    Create programming languages folder...

---
+++ programming languages/java/helloworld/Helloworld.java
@@ -0,0 +1,16 @@
+/*
+	@Program name: Print Helloworld! How are you? on console
+	@Date: 05/12/2020
+	@Author: Shailesh Fefar
+	@File name: Helloworld.java
+	@Licence Type: MIT License
+*/
+// Class Helloworld
+public class Helloworld
+{
+  public static void main(String []args)
+  {
+    // Print Helloworld! How are you? on console
+    System.out.println("Helloworld! How are you?");
+  }
+}
